afac243a240760712874f7025a7b24ffd2371b61
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-19 15:29:59 +0600

    completed making signup and signin forms

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return <h1>Please!Sign In</h1>;
+  const signInFormSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // Get Form value
+
+    const signInForm = event.target;
+    const email = signInForm.email.value;
+    const password = signInForm.password.value;
+    console.log(email, password);
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Sign In!</h1>
+        </div>
+        <div className="card card-body flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={signInFormSubmitHandler} className="">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <label className="label">
+              <button type="button" className="label-text-alt link link-hover">
+                Forgot password?
+              </button>
+            </label>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Signin
+              </button>
+            </div>
+          </form>
+          <p>
+            New to MW Hotel?
+            <Link to="/signup" className="btn btn-link p-0">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,89 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const SignUp = () => {
-  return <h1>Please!Sign Up</h1>;
+  const signUpFormSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // Get Form value
+
+    const signUpForm = event.target;
+    const email = signUpForm.email.value;
+    const password = signUpForm.password.value;
+    const confirmedPassword = signUpForm.confirm.value;
+    console.log(email, password, confirmedPassword);
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Sign In!</h1>
+        </div>
+        <div className="card card-body flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={signUpFormSubmitHandler} className="">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Confirm Password</span>
+              </label>
+              <input
+                type="password"
+                name="confirm"
+                placeholder="confirm password"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Signin
+              </button>
+            </div>
+          </form>
+
+          <div className="flex flex-col w-full">
+            <div className="grid place-items-center">
+              <p>
+                Existing Member of MW Hotel?
+                <Link to="/signin" className="btn btn-link p-0">
+                  Sign In
+                </Link>
+              </p>
+            </div>
+            <div className="divider">Or Sign up with</div>
+            <div className="grid  place-items-center">
+              <button className="btn btn-outline btn-primary">
+                <FcGoogle />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
