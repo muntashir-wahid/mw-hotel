fbcd5713045ece1e3a78c650c64fe08081ff9d87
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-19 13:06:29 +0600

    completed hero banner

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <h1>Welcome to Our Hotel!</h1>;
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url("https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">
+            Welcome to <span>MW Hotel</span>
+          </h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <div className="space-x-1.5 space-y-1.5">
+            <Link to="/available-rooms">
+              <button className="btn btn-primary">Available Rooms</button>
+            </Link>
+            <Link to="signin">
+              <button className="btn btn-primary">Get Sign In</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
